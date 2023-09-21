49d5402fffc66cebed3b3f226fc74b6a7788845e
Ember <git@emberdev.me>
2023-09-22 00:22:12 +1000

    aaa

--- app/models/status.rb
+++ app/models/status.rb
@@ -287,7 +287,7 @@ class Status < ApplicationRecord
       end
     end
 
-    ActiveRecord::Associations::Preloader.new.preload(record: records, :custom_emoji)
+    ActiveRecord::Associations::Preloader.new(record: records, :custom_emoji)
     # records
   end
 

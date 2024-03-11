0dafb9bf6f1cb3fd4674f23c1040936c83cc2ce2
Eugen Rochko <eugen@zeonfederated.com>
2024-03-11 09:57:07 +0100

    Add hints for rules (#29539)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_11_033014) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_10_123453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -879,6 +879,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_033014) do
     t.text "text", default: "", null: false
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
+    t.text "hint", default: "", null: false
   end
 
   create_table "scheduled_statuses", force: :cascade do |t|

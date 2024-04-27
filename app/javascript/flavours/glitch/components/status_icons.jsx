bf2a357657db6639195f54af0df267a49a5eace7
Ember <me@ember-is.gay>
2024-04-27 21:23:54 +1000

    a

--- app/javascript/flavours/glitch/components/status_icons.jsx
+++ app/javascript/flavours/glitch/components/status_icons.jsx
@@ -8,6 +8,7 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 
 import ExpandLessIcon from '@/material-icons/400-24px/expand_less.svg?react';
 import ForumIcon from '@/material-icons/400-24px/forum.svg?react';
+import CommentingIcon from '@/material-icons/400-24px/sms.svg?react';
 import HomeIcon from '@/material-icons/400-24px/home.svg?react';
 import ImageIcon from '@/material-icons/400-24px/image.svg?react';
 import InsertChartIcon from '@/material-icons/400-24px/insert_chart.svg?react';
@@ -28,6 +29,7 @@ const messages = defineMessages({
   previewCard: { id: 'status.has_preview_card', defaultMessage: 'Features an attached preview card' },
   pictures: { id: 'status.has_pictures', defaultMessage: 'Features attached pictures' },
   poll: { id: 'status.is_poll', defaultMessage: 'This toot is a poll' },
+  thread: { id: 'status.is_thread', defaultMessage: 'This toot is part of a thread' },
   video: { id: 'status.has_video', defaultMessage: 'Features attached videos' },
   audio: { id: 'status.has_audio', defaultMessage: 'Features attached audio files' },
   localOnly: { id: 'status.local_only', defaultMessage: 'Only visible from your instance' },
@@ -125,14 +127,23 @@ class StatusIcons extends PureComponent {
     return (
       <div className='status__info__icons'>
         {settings.get('language') && status.get('language') && <LanguageIcon language={status.get('language')} />}
-        {settings.get('reply') && status.get('in_reply_to_id', null) !== null ? (
-          <Icon
-            className='status__reply-icon'
-            id='comment'
-            icon={ForumIcon}
-            aria-hidden='true'
-            title={intl.formatMessage(messages.inReplyTo)}
-          />
+        {settings.get('reply') && status.get('in_reply_to_id', null) !== null ?
+          status.get('in_reply_to_account_id') === status.getIn(['account', 'id']) ? (
+            <Icon
+              className='status__reply-icon'
+              id='commenting'
+              icon={CommentingIcon}
+              aria-hidden='true'
+              title={intl.formatMessage(messages.thread)}
+            />
+          ) : (
+            <Icon
+              className='status__reply-icon'
+              id='comment'
+              icon={ForumIcon}
+              aria-hidden='true'
+              title={intl.formatMessage(messages.inReplyTo)}
+            />
         ) : null}
         {settings.get('local_only') && status.get('local_only') &&
           <Icon

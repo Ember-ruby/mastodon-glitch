11b96a198213452d0c7901cb5d36515f7d3608cc
Ember <me@ember-is.gay>
2024-04-28 12:38:02 +0000

    Update app/helpers/admin/trends/statuses_helper.rb

--- app/helpers/admin/trends/statuses_helper.rb
+++ app/helpers/admin/trends/statuses_helper.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
+# frozen_string_literal: true
+
 module Admin::Trends::StatusesHelper
   def one_line_preview(status)
     text = if status.local?
              status.text.split("\n").first
            else
              Nokogiri::HTML(status.text).css('html > body > *').first&.text
+           end
+
+    return '' if text.blank?
+
+    prerender_custom_emojis(h(text), status.emojis)
+  end
+  
+  def multi_line_preview(status)
+    text = if status.local?
+             status.text.split("\n")
+           else
+             Nokogiri::HTML(status.text).css('html > body > *')&.text
            end
 
     return '' if text.blank?

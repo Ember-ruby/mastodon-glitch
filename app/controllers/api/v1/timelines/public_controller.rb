d608edcd4a9b16e9334657e56a1094398b2a68db
Ember <me@ember-is.gay>
2024-06-29 14:40:57 +1000

    hm

--- app/controllers/api/v1/timelines/public_controller.rb
+++ app/controllers/api/v1/timelines/public_controller.rb
@@ -14,9 +14,7 @@ class Api::V1::Timelines::PublicController < Api::V1::Timelines::BaseController
   private
 
   def require_auth?
-    !Setting.timeline_preview
-    !Setting.timeline_preview_local && !truthy_param?(:local)
-    !Setting.timeline_preview_remote && !truthy_param?(:remote)
+    !Setting.timeline_preview || !Setting.timeline_preview_local && truthy_param?(:local) || !Setting.timeline_preview_remote && truthy_param?(:remote)
   end
 
   def require_auth_local?

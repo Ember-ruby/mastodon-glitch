e662d4e83e31c203eb8d46cccd5de173cd5d87f1
Ember <me@ember-is.gay>
2024-06-29 20:53:22 +1000

    remove junk i added

--- app/helpers/admin/settings/discovery_helper.rb
+++ app/helpers/admin/settings/discovery_helper.rb
@@ -12,16 +12,4 @@ module Admin::Settings::DiscoveryHelper
   def discovery_recommended_value
     authorized_fetch_overridden? ? :overridden : nil
   end
-
-  def public_feed_auth?
-    Setting.timeline_preview
-  end
-
-  def public_timelines_recommended_value
-    public_feed_auth? ? :overridden : nil
-  end
-
-  def public_timelines_warning_hint_text
-    public_feed_auth? ? t('admin.settings.security.public_timelines_warning_hint') : nil
-  end
 end

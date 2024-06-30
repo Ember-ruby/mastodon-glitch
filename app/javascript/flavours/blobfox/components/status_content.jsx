d3fba406a1cbb1b311b373e86d940a8c16953443
Ember <me@ember-is.gay>
2024-07-01 00:26:48 +1000

    h

--- app/javascript/flavours/blobfox/components/status_content.jsx
+++ app/javascript/flavours/blobfox/components/status_content.jsx
@@ -424,6 +424,7 @@ class StatusContent extends PureComponent {
 
       return (
         <div className={classNames} tabIndex={0} onMouseDown={this.handleMouseDown} onMouseUp={this.handleMouseUp} style={{ height: `${collapsedHeight}px` }}>
+          <div>{collapsed}</div>
           <p
             style={{ marginBottom: hidden && status.get('mentions').isEmpty() ? '0px' : null }}
           >

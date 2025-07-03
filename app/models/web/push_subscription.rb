1a022800e318d9a2fbdeb7fb572eef87afd985ac
Ember <me@ember-is.gay>
2025-07-03 18:58:28 +0000

    Revert "Fix add validation to webpush subscription keys (#30542)"
    This reverts commit cb1a0f4d8a8830a41fd8b82ea14458f2a3de0bcf.

--- app/models/web/push_subscription.rb
+++ app/models/web/push_subscription.rb
@@ -25,8 +25,6 @@ class Web::PushSubscription < ApplicationRecord
   validates :key_p256dh, presence: true
   validates :key_auth, presence: true
 
-  validates_with WebPushKeyValidator
-
   delegate :locale, to: :associated_user
 
   def pushable?(notification)

01a3542cf74d9c1af359f223055487bd7eb9be72
Ember <me@ember-is.gay>
2025-11-24 00:35:37 +0000

    Update config/initializers/regexp.rb

--- config/initializers/regexp.rb
+++ config/initializers/regexp.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-# 5s is a fairly high default, but that should account for slow servers under load
-Regexp.timeout = ENV.fetch('REGEXP_TIMEOUT', 5).to_f if Regexp.respond_to?(:timeout=)
+# 1s is a fairly high default, but that should account for slow servers under load
+Regexp.timeout = ENV.fetch('REGEXP_TIMEOUT', 1).to_f if Regexp.respond_to?(:timeout=)

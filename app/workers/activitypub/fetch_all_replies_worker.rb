carlosdgerez/mastodon

Add: Update root status when fetching replies (#34615)

app/workers/activitypub/fetch_all_replies_worker.rb
@@ -22,7 +22,7 @@ class ActivityPub::FetchAllRepliesWorker
     @root_status.touch(:fetched_replies_at)
     Rails.logger.debug { "FetchAllRepliesWorker - #{@root_status.uri}: Fetching all replies for status: #{@root_status}" }
 
-    uris_to_fetch, n_pages = get_replies(@root_status.uri, MAX_PAGES, options)
+    uris_to_fetch, n_pages = get_root_replies(@root_status.uri, options)
     return if uris_to_fetch.nil?
 
     fetched_uris = uris_to_fetch.clone.to_set
@@ -49,20 +49,39 @@ class ActivityPub::FetchAllRepliesWorker
 
   private
 
-  def get_replies(status_uri, max_pages, options = {})
-    replies_collection_or_uri = get_replies_uri(status_uri)
+  # @param status [String, Hash]
+  #   status URI, or the prefetched body of the Note object
+  def get_replies(status, max_pages, options = {})
+    replies_collection_or_uri = get_replies_uri(status)
     return if replies_collection_or_uri.nil?
 
-    ActivityPub::FetchAllRepliesService.new.call(status_uri, replies_collection_or_uri, max_pages: max_pages, **options.deep_symbolize_keys)
+    ActivityPub::FetchAllRepliesService.new.call(value_or_id(status), replies_collection_or_uri, max_pages: max_pages, **options.deep_symbolize_keys)
   end
 
-  def get_replies_uri(parent_status_uri)
-    fetch_resource(parent_status_uri, true)&.fetch('replies', nil)
+  # Get the URI of the replies collection of a status
+  #
+  # @param parent_status [String, Hash]
+  #   status URI, or the prefetched body of the Note object
+  def get_replies_uri(parent_status)
+    resource = parent_status.is_a?(Hash) ? parent_status : fetch_resource(parent_status, true)
+    resource&.fetch('replies', nil)
   rescue => e
-    Rails.logger.info { "FetchAllRepliesWorker - #{@root_status.uri}: Caught exception while resolving replies URI #{parent_status_uri}: #{e} - #{e.message}" }
+    Rails.logger.info { "FetchAllRepliesWorker - #{@root_status.uri}: Caught exception while resolving replies URI #{parent_status}: #{e} - #{e.message}" }
     # Raise if we can't get the collection for top-level status to trigger retry
-    raise e if parent_status_uri == @root_status.uri
+    raise e if value_or_id(parent_status) == @root_status.uri
 
     nil
+  end
+
+  # Get the root status, updating the status without fetching it twice
+  #
+  # @param root_status_uri [String]
+  def get_root_replies(root_status_uri, options = {})
+    root_status_body = fetch_resource(root_status_uri, true)
+    raise RuntimeError("FetchAllRepliesWorker - #{@root_status.uri}: Root status could not be fetched") if root_status_body.nil?
+
+    FetchReplyWorker.perform_async(root_status_uri, { **options, prefetched_body: root_status_body })
+
+    get_replies(root_status_body, MAX_PAGES, options)
   end
 end

Ember-ruby/mastodon-glitch

app/workers/activitypub/fetch_all_replies_worker.rb
@@ -19,7 +19,7 @@ class ActivityPub::FetchAllRepliesWorker
     @parent_status = Status.find(parent_status_id)
     Rails.logger.debug { "FetchAllRepliesWorker - #{@parent_status.uri}: Fetching all replies for status: #{@parent_status}" }
 
-    uris_to_fetch, n_pages = get_replies(@parent_status.uri, MAX_PAGES, options)
+    uris_to_fetch, n_pages = get_root_replies(@parent_status.uri, options)
     return if uris_to_fetch.nil?
 
     @parent_status.touch(:fetched_replies_at)
@@ -46,31 +46,39 @@ class ActivityPub::FetchAllRepliesWorker
 
   private
 
-  def get_replies(status_uri, max_pages, options = {})
-    replies_collection_or_uri = get_replies_uri(status_uri)
+  # @param status [String, Hash]
+  #   status URI, or the prefetched body of the Note object
+  def get_replies(status, max_pages, options = {})
+    replies_collection_or_uri = get_replies_uri(status)
     return if replies_collection_or_uri.nil?
 
-    ActivityPub::FetchAllRepliesService.new.call(status_uri, replies_collection_or_uri, max_pages: max_pages, **options.deep_symbolize_keys)
+    ActivityPub::FetchAllRepliesService.new.call(value_or_id(status), replies_collection_or_uri, max_pages: max_pages, **options.deep_symbolize_keys)
   end
 
-  def get_replies_uri(parent_status_uri)
-    begin
-      json_status = fetch_resource(parent_status_uri, true)
-      if json_status.nil?
-        Rails.logger.debug { "FetchAllRepliesWorker - #{@parent_status.uri}: error getting replies URI for #{parent_status_uri}, returned nil" }
-        nil
-      elsif !json_status.key?('replies')
-        Rails.logger.debug { "FetchAllRepliesWorker - #{@parent_status.uri}: no replies collection found in ActivityPub object: #{json_status}" }
-        nil
-      else
-        json_status['replies']
-      end
-    rescue => e
-      Rails.logger.warn { "FetchAllRepliesWorker - #{@parent_status.uri}: caught exception fetching replies URI: #{e}" }
-      # Raise if we can't get the collection for top-level status to trigger retry
-      raise e if parent_status_uri == @parent_status.uri
+  # Get the URI of the replies collection of a status
+  #
+  # @param parent_status [String, Hash]
+  #   status URI, or the prefetched body of the Note object
+  def get_replies_uri(parent_status)
+    resource = parent_status.is_a?(Hash) ? parent_status : fetch_resource(parent_status, true)
+    resource&.fetch('replies', nil)
+  rescue => e
+    Rails.logger.info { "FetchAllRepliesWorker - #{@root_status.uri}: Caught exception while resolving replies URI #{parent_status}: #{e} - #{e.message}" }
+    # Raise if we can't get the collection for top-level status to trigger retry
+    raise e if value_or_id(parent_status) == @root_status.uri
 
-      nil
-    end
+    nil
+  end
+
+  # Get the root status, updating the status without fetching it twice
+  #
+  # @param root_status_uri [String]
+  def get_root_replies(root_status_uri, options = {})
+    root_status_body = fetch_resource(root_status_uri, true)
+    raise RuntimeError("FetchAllRepliesWorker - #{@root_status.uri}: Root status could not be fetched") if root_status_body.nil?
+
+    FetchReplyWorker.perform_async(root_status_uri, { **options, prefetched_body: root_status_body })
+
+    get_replies(root_status_body, MAX_PAGES, options)
   end
 end